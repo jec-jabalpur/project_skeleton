f44cb52ece9f907fdab980b3742f71c1c423cbb6
unmesh-shukla <unmesh.shukla.personal@gmail.com>
2020-07-11 10:31:59 +0530

    Basic Setup #1

---
+++ template.js
@@ -0,0 +1,12 @@
+export default () => {
+    return `<!DOCTYPE html>
+    <html>
+    <head>
+        <title>Project Skeleton</title>
+    </head>
+    <body>
+        <div id="root"></div>
+    </body>
+    </html>
+    `
+}

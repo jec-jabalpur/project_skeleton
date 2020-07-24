449fc57372a2d392624dc8bb4092d3a7ce224249
unmesh-shukla <unmesh.shukla.personal@gmail.com>
2020-07-24 18:45:30 +0530

    Fixed bug - invalid error response message

--- server/helpers/dbErrorHandler.js
+++ server/helpers/dbErrorHandler.js
@@ -1,8 +1,16 @@
 const getUniqueErrorMessage = (err) => {
     let custom_message;
     try {
-        let field_name = err.message.substring(err.message.lastIndexOf('.$') + 2, err.message.lastIndexOf('_1'));
-        console.log(field_name);
+        let field_name = "";
+        let field_name_end = err.message.lastIndexOf('_1') - 1;
+        for (let i = field_name_end; i >= 0; i--) {
+            if (err.message[i] !== ' ') {
+                field_name = err.message[i] + field_name;
+            }
+            else {
+                break;
+            }
+        }
         custom_message = field_name.charAt(0).toUpperCase() + field_name.slice(1) + ' already exists!';
     }
     catch(ex) {
@@ -19,7 +27,7 @@ const getErrorMessage = (err) => {
         switch(err.code) {
             case 11000:
             case 11001:
-                message: getUniqueErrorMessage(err)
+                message = getUniqueErrorMessage(err)
                 break
             default:
                 message = 'Something went wrong!'
